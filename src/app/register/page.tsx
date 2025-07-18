c8df1a44c0eb8df78c72c7645f2eb09b8d882482
Mohaiminul375 <mohaiminul375@gmail.com>
2025-07-18 21:31:22 +0600

    Initial commit for new project

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
+import { Label } from "@/components/ui/label";
+// import { Label } from "@radix-ui/react-label";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
